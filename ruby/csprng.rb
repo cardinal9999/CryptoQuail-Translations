6348eabb9c6215d5afd3432df9b0a18bb4f820c8
cardinal9999 <86199598+cardinal9999@users.noreply.github.com>
2021-10-15 21:39:38 -0400

    Update csprng.rb

--- ruby/csprng.rb
+++ ruby/csprng.rb
@@ -1,13 +1,22 @@
 require "securerandom"
 def randint(min, max)
-	SecureRandom.send(:choose, (min..max), 8)
+	SecureRandom.random_number(max - min) + min
 end
 def password(len)
 	chars = ('a'..'z').to_a + ('0'..'9').to_a + ["!", "@", "#", "$", "%"]
-	len.times.map { chars[SecureRandom.random_number(chars.length)] }.join
+	len.times.map { chars[randint(0, chars.length)] }.join
 end
-
+def shuffle(list)
+	list.shuffle(random: Random.new(randint(1, 10000)))
+end
+def token_hex(len)
+	s = SecureRandom.hex(len)
+	randindex = (s.size.to_f / 2).ceil
+	s[0, randindex]
+end
 if __FILE__ == $0
-	puts randint(1, 1111111)
-	puts password(11)
+	puts randint(1, 10000)
+	puts password(10)
+	puts token_hex(12)
+	puts shuffle(["DogeScript", "JavaScript", "Go", "Ruby"])
 end
